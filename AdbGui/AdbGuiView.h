e2a8ba7527d15d2412a3bb956b2d4b7f5fa589e9
singun-lxd <liuxindian@gmail.com>
2017-05-19 14:22:52 +0800

    + Add an empty WTL project

---
+++ AdbGui/AdbGuiView.h
@@ -0,0 +1,25 @@
+// AdbGuiView.h : interface of the CAdbGuiView class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CAdbGuiView : public CWindowImpl<CAdbGuiView, CListViewCtrl>
+{
+public:
+	DECLARE_WND_SUPERCLASS(NULL, CListViewCtrl::GetWndClassName())
+
+	BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		pMsg;
+		return FALSE;
+	}
+
+	BEGIN_MSG_MAP(CAdbGuiView)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+};
