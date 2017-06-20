4b59faeab09cdc9b1be588aeb22327abaa61e63c
singun-lxd <liuxindian@gmail.com>
2017-06-21 00:55:54 +0800

    * localized ribbon control

--- AdbWinGui/AdbWinGui.cpp
+++ AdbWinGui/AdbWinGui.cpp
@@ -59,6 +59,9 @@ int Run(LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
 {
+	// set language to english
+	::SetThreadPreferredUILanguages(MUI_LANGUAGE_NAME, _T("en-US"), NULL);
+
 	HRESULT hRes = ::CoInitialize(NULL);
 // If you are running on NT 4.0 or higher you can use the following call instead to 
 // make the EXE free threaded. This means that calls come in on a random RPC thread.
